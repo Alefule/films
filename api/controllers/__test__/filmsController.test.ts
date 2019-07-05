f28771c047d5766e506b63333ce525df0229dee9
raulanatol <raul@natol.es>
2019-07-05 18:08:35 +0100

    🚀 test almost finish

--- api/controllers/__test__/filmsController.test.ts
+++ api/controllers/__test__/filmsController.test.ts
@@ -1,5 +1,11 @@
-describe('Probe Films Controller', () => {
-    test('should ', () => {
-      
-    }); 
+import { showFilms } from "../filmsController";
+
+describe('filmsController', () => {
+    describe('showFilms()', () => {
+        test('should return movies ids and names', () => {
+            const movies = showFilms();
+            expect(Object.keys(movies)).toEqual(['id','name']);
+            console.log(movies);
+        });
+    });
 });
--- api/controllers/filmsController.ts
+++ api/controllers/filmsController.ts
@@ -1,42 +1,45 @@
 const Film = require('../models/Film');
 const films = require('../../data/json/films.json');
 
-exports.showFilms = (req,res) => res.json(films);
+export const showFilms = () => films.map(movie => {
+    const newMovie = {...movie};
+    delete newMovie.likes;
+    return newMovie;
+});
 
-exports.showOneFilm = (req,res) => {
+exports.showOneFilm = (req, res) => {
     let id = req.params.id;
     let film = films.find(film => film.id == id);
     res.send(film);
-}
+};
 
 exports.addFilm = (req, res) => {
-    let film = {...req.body}
-    let filmToAdd = new Film(film.id,film.name);
+    let film = { ...req.body };
+    let filmToAdd = new Film(film.id, film.name);
     films.push(filmToAdd);
     res.json(films);
-}
+};
 
 exports.deleteFilm = (req, res) => {
     let idToDelete = req.params.id;
     for (let i = 0; i < films.length; i++) {
         if (films[i].id == idToDelete) {
-            films.splice(i,1);
+            films.splice(i, 1);
         }
     }
     res.json(films);
-}
+};
 
-exports.updateFilm = (req, res) =>{
-    let idToUpadte = req.params.id;
-    let filmToUpdate = {...req.body};
+exports.updateFilm = (req, res) => {
+    let idToUpdate = req.params.id;
+    let filmToUpdate = { ...req.body };
     for (let i = 0; i < films.length; i++) {
-        if(films[i].id == idToUpadte){
-            films[i].id = films[i].id;
+        if (films[i].id == idToUpdate) {
             films[i].name = filmToUpdate.name || films[i].name;
             films[i].likes = filmToUpdate.likes || films[i].likes;
         }
     }
-}
+};
 
 
 
